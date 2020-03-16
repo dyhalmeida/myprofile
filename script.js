ae02b053969e4b0fda209e35f90a2a3dbc977653
Diego Almeida <dyhalmeida@gmail.com>
2020-03-16 16:05:34 -0300

    embed vídeo modal

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const modalOverlay = document.querySelector('.modal-overlay');
-const cards = Array.from(document.querySelectorAll('.card'));
+(() => {
+  const modalOverlay = document.querySelector('.modal-overlay');
+  const cards = Array.from(document.querySelectorAll('.card'));
+  
+  cards.map(card => card.addEventListener('click', () => { 
+    
+    modalOverlay.classList.add('active');
+    const [,,,,videoID] = card.querySelector('img').src.split('/');
+    modalOverlay.querySelector('iframe').src = `https://www.youtube.com/embed/${videoID}`;
+  
+  }));
+  
+  const closeModal = document.querySelector('.close-modal');
+  closeModal.addEventListener('click', () => { 
+    modalOverlay.querySelector('iframe').src = '';
+    modalOverlay.classList.remove('active');
+  });
+})();
 
-cards.map(card => card.addEventListener('click', () => modalOverlay.classList.add('active')))
-
-const closeModal = document.querySelector('.close-modal');
-closeModal.addEventListener('click', () => modalOverlay.classList.remove('active'));
